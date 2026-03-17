dbe656a653d3a0badbd62750a81057a370f08b51
Melanie Rodriguez <melarodrigg@gmail.com>
2026-03-16 22:30:54 -0600

    added linked list and recursion functions to sum node data, reverse the list, and search a given id

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ if __name__ == "__main__":
     """
 
     # TODO: 1) Create a LinkedList instance
-    
+    llexample = LinkedList()
 
     # TODO: 2) Insert some sample data using insert_at_front or insert_at_end
+    llexample.insert_at_front(1)
+    llexample.insert_at_front(2)
+    llexample.insert_at_front(3)
+    llexample.insert_at_front(4)
     
     # TODO: 3) Display the list to verify insertion
-    
+    llexample.display()
 
     # TODO: 4) Call recursive_sum and print the result
-    
+    print(llexample.recursive_sum())
 
     # TODO: 5) Call recursive_search with a target and print result
-    
+    print(f"Recursive_sarch: {llexample.recursive_search(3)} ")
 
     # TODO: 6) Call recursive_reverse, then display the reversed list
-    
-
+    print(llexample.recursive_reverse())
 
 # 
